18abbf00cafabc5d7faf8256e9fad55232323177
vijayaraju <112242459+192111282@users.noreply.github.com>
2022-09-26 10:04:46 +0530

    Add files via upload

---
+++ bread program.py
@@ -0,0 +1,9 @@
+fresh=float(input("enter the number of fresh leaves"))
+old=float(input("enter the number of day old leaves"))
+amt_of_fresh=fresh*185.00
+amt_of_old=old*185*.6
+print("regular_price=185.00")
+print("amount of fresh leaves",amt_of_fresh)
+print("amount of old leaves",amt_of_old)
+print("Total Amount to be paid",amt_of_fresh+
+amt_of_old)
